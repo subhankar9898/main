705e109acc2c8640330d05d74276bae2e2f9d91d
subhankar9898 <subhankarprusty25@gmail.com>
2024-08-02 00:32:53 +0530

    Add files via upload

---
+++ src/routes/pricing/+page.svelte
@@ -0,0 +1,134 @@
+    <script>
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section>
+        <div
+        class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+    ><div>
+        <br>
+        <h1 class={" text-7xl sm:text-7xl text-center "}>
+            Pricing
+        </h1>
+        
+            <h6 class="text-large sm:text-xl md:text-2xl text-center">
+            <br>Start for free and scale as you grow.
+         </h6>
+        
+    </div>
+    <div class="duration-200 sm:text-x1 md:text-1x1 text-centre space-x-2">
+        <button>
+            <h6 class="relative z-9">
+                Transactional Email
+            </h6>
+        </button>
+        <button>
+            <h6 class="relative z-9">
+                Marketing Email
+            </h6>
+        </button>
+        <br>
+        <br>
+
+
+    </div>
+    <div>
+        <h3 class={" text-4xl sm:text-4xl text-left poppins "}>
+            Add-ons 
+        </h3>
+        <br>
+        <br>
+        <div class="box-border h-100 w-100 p-5 border-1">
+
+            <h3 class={" text-1xl sm:text-2xl  "}>
+                Dedicated IPs - $30 / mo
+            </h3>
+            <br>
+            Our fully managed dedicated IP's are a great way to obtain complete control of your reputation. We take great care to maintain good-standing shared IP's, but for some high-volume senders, you can't leave anything up to chance. Request a dedicated IP and we will warmup, monitor, and autoscale it based on your utilization so you can focus on one thing, sending.
+            <br>
+            <br>
+            
+            <button
+            class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
+          >
+            <div
+              class=" top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+            />
+            <h4 class="relative z-9">Request Dedicated IP</h4>
+          </button>
+        <br> 
+        <br>
+    </div>
+        </div>
+        <img
+        src={"images/Trust.png"}
+        alt="Trust Company"/>
+        <br>
+        <br>
+    </div>
+    <div>
+       <br> <h3 class={" text-3xl sm:text-3xl font-semibold "}>
+            Frequently Asked Questions
+        </h3>
+    </div>
+    <br>
+    <br>
+    <div>
+        <button
+        class="blueShadow relative overflow-hidden px-1 py-77 "
+        >
+           <br> <h4 class="relative z-9"> >Do you offer any discounts for annual subscriptons? </h4></button>
+        <br>
+        <br>
+        <button
+        class="blueShadow relative overflow-hidden px-1 py-77 "
+        >
+        <h4 class="relative z-9"> >What payment methods do you accept ? </h4></button>
+        <br>
+        <br>
+        <button
+        class="blueShadow relative overflow-hidden px-1 py-77"
+        >
+        <h4 class="relative z-9"> > Is there a free trial available ? </h4></button>
+        <br><br>
+        <button
+        class="blueShadow relative overflow-hidden px-1 py-77"
+        >
+        <h4 class="relative z-9"> >Are there any limits on the number of team members ? </h4></button>
+        <br><br>
+        <button
+        class="blueShadow relative overflow-hidden px-1 py-77 "
+        >
+        <h4 class="relative z-9"> >Who can i contact about a custom plan? </h4></button>
+        <br><br>
+        <button
+        class="blueShadow relative overflow-hidden px-1 py-77"
+        >
+        <h4 class="relative z-9"> >What happens if i send more emails than my plan allows? </h4></button>
+        <br><br>
+        </div>
+        <br><br>
+        <div>
+            <div class="grid place-items-center">
+                <img src ={"images/R-Pricing.png"} alt="R Logo"/></div>
+        <div class="flex flex-col items-center justify-center px-4 py-32 ">
+            <h2 class="font-book font-styling font-display font-effect-hero mb-2 text-center text-[3rem] md:text-[3.5rem] tracking-tight leading-[120%] font-gradient">
+                "Start sending tonight"
+            </h2>
+            <p class="sans mb-8 text-center text-base md:text-[1.125rem] md:leading-[1.5] text-slate-11 font-normal">
+                Resend delivers mission-critical emails for some of the fastest growing<br> teams.
+            </p>
+            <button
+            class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
+          >
+            <div
+              class=" top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+            />
+            <h4 class="relative z-9">Sign up for free</h4>
+          </button>
+
+        </div>
+
+        </div>
+   </section>
+   </main>
